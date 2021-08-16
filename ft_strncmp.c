017516f0b72839d46ecf6484b618c1a7cb6e5277
Nicholas Khoo <xmencool1@gmail.com>
2021-08-16 15:35:51 +0800

    Final

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,23 +6,22 @@
 /*   By: nkay-hoo <nkay-hoo@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 02:21:16 by nkay-hoo          #+#    #+#             */
-/*   Updated: 2021/08/12 03:41:18 by nkay-hoo         ###   ########.fr       */
+/*   Updated: 2021/08/16 15:20:05 by nkay-hoo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *str1, const char *str2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	count;
+	size_t	i;
 
-	count = 0;
-	while ((str1[count] != '\0' || str2[count] != '\0') && count < n)
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (str1[count] != str2[count])
-			return (*(unsigned char *)(str1 + count) -
-			 *(unsigned char *)(str2 + count));
-		count++;
+		if (s1[i] != s2[i])
+			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+		i++;
 	}
 	return (0);
 }
@@ -31,8 +30,8 @@ int	main(int argc, char **argv)
 {
 	if (argc <= 1)
 		return (0);
-	printf("%d\n", ft_strncmp(argv[1], argv[2], 3));
-	printf("%d\n", strncmp(argv[1], argv[2], 3));
+	printf("%d\n", ft_strncmp(argv[1], argv[2], 5));
+	printf("%d\n", strncmp(argv[1], argv[2], 5));
 	return (0);
 }
 */
